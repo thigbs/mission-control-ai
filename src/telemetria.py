6d8c93665fab9f36a28f196de8ecffdf9260aa05
thigbs <thiagogarciabs@gmail.com>
2026-06-09 18:11:16 -0300

    Create telemetria.py

---
+++ src/telemetria.py
@@ -0,0 +1,18 @@
+import random
+
+def coletar():
+    return {
+        "ndvi": round(random.uniform(0.3, 0.9), 2),
+        "temperatura": random.randint(20, 90),
+        "armazenamento": random.randint(10, 100),
+        "estabilidade": random.randint(70, 100)
+    }
+    import random
+
+def coletar():
+    return {
+        "ndvi": round(random.uniform(0.3, 0.9), 2),
+        "temperatura": random.randint(20, 90),
+        "armazenamento": random.randint(10, 100),
+        "estabilidade": random.randint(70, 100)
+    }
